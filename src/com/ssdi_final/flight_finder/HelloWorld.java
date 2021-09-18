1a20e02ff77089d7e423d35e4b0478e8cdb5ab5f
new-dark-knightt <amedi@uncc.edu>
2021-09-18 11:42:00 -0400

    sample

---
+++ src/com/ssdi_final/flight_finder/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ssdi_final.flight_finder;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
